7c6206f37b538a088d93e6c562cc5e0fff0f5100
LUFT-AQUILA <luftaquila@protonmail.ch>
2019-08-28 03:37:37 +0900

    Add files via upload

---
+++ resources/browserCheck.js
@@ -0,0 +1,20 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'UA-119383265-1');
+if(browserVersionCheck().version > 0 && browserVersionCheck().version < 12) alert('인터넷 익스플로러는 지원하지 않는 오래된 브라우저입니다.\n크롬, 파이어폭스, 오페라, 엣지 등 다른 브라우저를 사용하십시오.');
+function browserVersionCheck() {
+  var word;
+  var agent = navigator.userAgent.toLowerCase();
+  var info = {  name: "N/A" , version: -1  };
+  if (navigator.appName == "Microsoft Internet Explorer") word = "msie ";
+  else if (agent.search("trident") > -1) word = "trident/.*rv:";
+  else if (agent.search("edge/") > -1) word = "edge/";
+  else return info;
+  var reg = new RegExp( word + "([0-9]{1,})(\\.{0,}[0-9]{0,1})" );
+  if (reg.exec(agent) != null) {
+    info.version = parseFloat(RegExp.$1 + RegExp.$2);
+    info.name = (word == "edge/") ? "Edge" : "IE";
+  }
+  return info;
+}
---
+++ resources/main.js
@@ -0,0 +1,39 @@
+const input = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-btn");
+const expand = () => {
+  searchBtn.classList.toggle("close");
+  input.classList.toggle("square");
+};
+searchBtn.addEventListener("click", expand);
+
+$(function() {
+  if(Cookies.get('keyword')) $('#search-input').val(Cookies.get('keyword'));
+  $('#search-input').keypress(function(e) { if(e.which == 13) search($('#search-input').val()); });
+});
+
+function search(query) {
+  $.ajax({
+  contentType: "application/x-www-form-urlencoded;charset=euc-kr",
+  url:'https://script.google.com/macros/s/AKfycbz3L8IsK35IT2FUNMR8V4KfU2tsNpVWJxwGdOt3Nngz1ANhUac/exec?url=' + encodeURIComponent('https://www.google.com/search?q=' + query + '&tbm=nws') + '&callback=?',
+  type: "GET",
+  dataType: 'json',
+  cache: false,
+  success: function (response) {
+    console.log(response);
+    /*
+    var str = '', html = new DOMParser().parseFromString(response.result, "text/html").getElementsByTagName('table')[0].getElementsByTagName('tr');
+    for(var i = 0; i < html.length; i++) {
+      data[i] = Object.values(html[i].getElementsByTagName('td'));
+    }
+    data.splice(0, 2);
+    for(var i = 0; i < data.length; i++) {
+      data[i] = [data[i][0].textContent, data[i][5].textContent, data[i][12].textContent];
+      str += '<option value=' + data[i][0] + '>' + data[i][0] + '</option>';
+    }
+    $('#spot').html(str);
+    updateSpot();
+    updateAlt();
+  */
+  }
+});
+}
